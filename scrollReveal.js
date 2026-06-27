b7d85c4e05bc3d476ffc6964919c65b8afe254e4
joaocarlos-lima <joaolimaguarniere@gmail.com>
2026-06-27 19:33:02 -0300

    foi terminado o index e implementado o recurso de scroll reveal

---
+++ scrollReveal.js
@@ -0,0 +1,90 @@
+window.revelar = ScrollReveal({ reset: true });
+// topo
+revelar.reveal('.inicialPage-imagem', {
+    duration: 700,
+    distance: '90px',
+    origin: 'right'
+});
+revelar.reveal('#titulo-inicial-page', {
+    duration: 700,
+    distance: '90px',
+    origin: 'left'
+});
+revelar.reveal('#subtitulo-inicial-page', {
+    duration: 700,
+    distance: '90px',
+    origin: 'left'
+});
+revelar.reveal('#button-cadastro-page', {
+    duration: 900,
+    distance: '90px',
+    origin: 'left'
+});
+revelar.reveal('#button-como-funciona', {
+    duration: 1100,
+    distance: '90px',
+    origin: 'left'
+});
+
+
+//recursos
+
+revelar.reveal('.top-texts', {
+    duration: 900,
+    distance: '90px',
+    origin: 'left'
+});
+revelar.reveal('.container-recursos', {
+    duration: 900,
+    distance: '90px',
+    origin: 'bottom'
+});
+
+//passo a passo
+revelar.reveal('.comece-a-usar-imagem', {
+    duration: 900,
+    distance: '90px',
+    origin: 'right'
+});
+revelar.reveal('.comecar-text', {
+    duration: 900,
+    distance: '90px',
+    origin: 'bottom'
+});
+revelar.reveal('.passos', {
+    duration: 900,
+    distance: '90px',
+    origin: 'bottom'
+});
+
+//Sobre nos
+revelar.reveal('.titulo-bolha-introducao', {
+    duration: 900,
+    distance: '90px',
+    origin: 'bottom'
+});
+revelar.reveal('.conteiner-sobrenos', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom'
+});
+revelar.reveal('.imagemSobreNos', {
+    duration: 800,
+    distance: '90px',
+    origin: 'right'
+});
+revelar.reveal('.texto-subtitulo', {
+    duration: 900,
+    distance: '90px',
+    origin: 'bottom'
+});
+revelar.reveal('.comparacao-conteiner-branco', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left'
+});
+revelar.reveal('.comparacao-conteiner-azul', {
+    duration: 1200,
+    distance: '90px',
+    origin: 'right'
+});
